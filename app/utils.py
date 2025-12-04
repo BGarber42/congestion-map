d1d9c6352ba648b0639ea9ffd0ac284f13157469
Brent Garber <me@bgarber.work>
2025-12-04 16:22:09 -0500

    Abstract to reduce duplication

---
+++ app/utils.py
@@ -0,0 +1,17 @@
+from typing import Any, Dict, Optional
+
+from app.models import PingPayload
+
+
+def get_mock_ping_request(overrides: Optional[Dict[str, Any]] = None) -> PingPayload:
+    ping_data = {
+        "device_id": "abc123",
+        "timestamp": "2025-01-01T12:34:56Z",
+        "lat": 40.743,
+        "lon": -73.989,
+    }
+
+    if overrides:
+        ping_data.update(overrides)
+
+    return PingPayload(**ping_data)
