002b4cfb770642d3b99d1ec8f3f85787a46e7ab5
Brent Garber <me@bgarber.work>
2025-12-04 16:29:02 -0500

    Convert other tests

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -32,54 +32,39 @@ class TestPingEndpoint:
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
 
     async def test_invalid_longitude(self, async_client: AsyncClient) -> None:
-        ping_payload = {
-            "device_id": "abc123",
-            "timestamp": "2025-01-01T12:34:56Z",
-            "lat": 40.743,
-            "lon": 181,
-        }
+        ping_payload = get_mock_ping_request({"lon": 181}, return_instance=False)
 
         response = await async_client.post("/ping", json=ping_payload)
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
 
     async def test_missing_device_id(self, async_client: AsyncClient) -> None:
-        ping_payload = {
-            "timestamp": "2025-01-01T12:34:56Z",
-            "lat": 40.743,
-            "lon": -73.989,
-        }
+        ping_payload = get_mock_ping_request({"device_id": ""}, return_instance=False)
 
         response = await async_client.post("/ping", json=ping_payload)
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
 
     async def test_missing_timestamp(self, async_client: AsyncClient) -> None:
-        ping_payload = {"device_id": "abc123", "lat": 40.743, "lon": -73.989}
+        ping_payload = get_mock_ping_request({"timestamp": None}, return_instance=False)
 
         response = await async_client.post("/ping", json=ping_payload)
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
 
     async def test_not_a_timestamp(self, async_client: AsyncClient) -> None:
-        ping_payload = {
-            "device_id": "abc123",
-            "timestamp": "not a timestamp",
-            "lat": 40.743,
-            "lon": -73.989,
-        }
+        ping_payload = get_mock_ping_request(
+            {"timestamp": "not a timestamp"}, return_instance=False
+        )
 
         response = await async_client.post("/ping", json=ping_payload)
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
 
     async def test_not_real_coordinates(self, async_client: AsyncClient) -> None:
-        ping_payload = {
-            "device_id": "abc123",
-            "timestamp": "2025-01-01T12:34:56Z",
-            "lat": "somestring",
-            "lon": ["not a number"],
-        }
+        ping_payload = get_mock_ping_request(
+            {"lat": "somestring", "lon": ["not a number"]}, return_instance=False
+        )
 
         response = await async_client.post("/ping", json=ping_payload)
 
